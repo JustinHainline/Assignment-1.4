f4be9cbbceae349a1820ea604007406e4b6e2fba
justi <justi@DESKTOP-UC4ESQF>
2024-05-09 10:41:22 -0500

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,28 @@
+﻿namespace Assignment_1._4
+{
+    
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Point p1 = new Point();
+            Point p2 = new Point();
+            if (p1.x == p2.x)
+            {
+                Console.WriteLine("Points are on the same axis.");
+            }
+            else
+            {
+                Console.WriteLine("Points are not on the same axis.");
+            }
+            
+        }
+        public struct Point
+        {
+           public double x;
+           public double y;
+
+
+        }
+    }
+}
